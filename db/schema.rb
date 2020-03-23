80b15c56ded7c5ad6ff403fbed293d9db10aecb1
Regina Campos <rmscampos@gmail.com>
2020-03-23 15:05:47 -0500

    updates comments model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_14_220943) do
+ActiveRecord::Schema.define(version: 2020_03_20_185530) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
